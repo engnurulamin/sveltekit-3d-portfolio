83d8f87794e9e7a9f91c8cf1acd6b309de116aa8
Nurul Amin <namin2750@gmail.com>
2024-07-25 23:02:06 +0600

    name animation

--- src/lib/slices/Hero/index.svelte
+++ src/lib/slices/Hero/index.svelte
@@ -1,8 +1,62 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
 	import Scene from './Scene.svelte';
+	import { onMount } from 'svelte';
+	import gsap from 'gsap';
 
 	export let slice: Content.HeroSlice;
+
+	const first_name_letters = slice.primary.first_name?.split('') ?? '';
+	const last_name_letters = slice.primary.last_name?.split('') ?? '';
+
+	onMount(() => {
+		const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+		if (prefersReducedMotion) {
+			gsap.to('.name-animation', { opacity: 1 });
+			gsap.to('.job-title', { opacity: 1 });
+			return;
+		}
+
+		const tl = gsap.timeline();
+
+		tl.fromTo(
+			'.name-animation',
+			{
+				x: -100,
+				opacity: 0,
+				rotate: -10
+			},
+			{
+				x: 0,
+				rotate: 0,
+				opacity: 1,
+				ease: 'elastic.out(1,0.3)',
+				duration: 1,
+				transformOrigin: 'left top',
+				delay: 0.5,
+				stagger: {
+					each: 0.1,
+					from: 'random'
+				}
+			}
+		);
+
+		tl.fromTo(
+			'.job-title',
+			{
+				y: 20,
+				opacity: 0,
+				scale: 1.2
+			},
+			{
+				opacity: 1,
+				y: 0,
+				duration: 1,
+				scale: 1,
+				ease: 'elastic.out(1,0.3)'
+			}
+		);
+	});
 </script>
 
 <section
@@ -20,16 +74,23 @@
 			<div class="col-start-1 md:row-start-1">
 				<h1
 					class="mb-2 md:mb-8 text-[clamp(3rem,20vmin,13rem)] font-extrabold leading-none tracking-tighter text-nowrap"
+					aria-label={slice.primary.first_name + ' ' + slice.primary.last_name}
 				>
-					<span class="block text-slate-300">
-						{slice.primary.first_name}
-					</span>
-					<span class="block text-slate-500 -mt-[.2em]">
-						{slice.primary.last_name}
-					</span>
+					{#if first_name_letters.length && last_name_letters.length}
+						<span class="block text-slate-300">
+							{#each first_name_letters as letter}
+								<span class="name-animation inline-block opacity-0">{letter}</span>
+							{/each}
+						</span>
+						<span class="block text-slate-500 -mt-[.2em]">
+							{#each last_name_letters as letter}
+								<span class="name-animation inline-block opacity-0">{letter}</span>
+							{/each}
+						</span>
+					{/if}
 				</h1>
 				<span
-					class="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-transparent text-2xl font-bold uppercase tracking-[.2em] md:text-4xl"
+					class="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-transparent text-2xl font-bold uppercase tracking-[.2em] md:text-4xl opacity-0"
 				>
 					{slice.primary.tag_line}
 				</span>
