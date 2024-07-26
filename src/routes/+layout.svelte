26ac053a1e556efa46ce570fed0ab0d463b37a8c
Nurul Amin <namin2750@gmail.com>
2024-07-26 18:01:33 +0600

    fix hero background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,14 @@
 		<meta name="twitter:card" content="summary_large_image" />
 	{/if}
 </svelte:head>
+
+<header>Header</header>
 <main>
 	<slot />
 </main>
+<div class="background-gradient absolute inset-0 -z-50 max-h-screen"></div>
+<div
+	class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"
+></div>
+<footer>Footer</footer>
 <PrismicPreview {repositoryName} />
