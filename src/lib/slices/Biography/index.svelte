ceca339d66b5bd29cdf96c93bab0d41910efd56f
Nurul Amin <namin2750@gmail.com>
2024-07-30 21:03:08 +0600

    about page wrap into Bounded com

--- src/lib/slices/Biography/index.svelte
+++ src/lib/slices/Biography/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Bounded from '$lib/components/Bounded.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import type { Content } from '@prismicio/client';
 	import { PrismicImage, PrismicRichText } from '@prismicio/svelte';
@@ -6,7 +7,7 @@
 	export let slice: Content.BiographySlice;
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+<Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	<div class="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
 		<h1>{slice.primary.heading}</h1>
 		<div class="col-start-1 prose prose-invert prose-slate prose-xl">
@@ -19,4 +20,4 @@
 			imgixParams={{ q: 90 }}
 		/>
 	</div>
-</section>
+</Bounded>
