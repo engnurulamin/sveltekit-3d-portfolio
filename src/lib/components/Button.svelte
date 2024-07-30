14009c26749ea9e97c8b6f63adc8a34536f6b046
Nurul Amin <namin2750@gmail.com>
2024-07-30 16:23:26 +0600

    fix

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -16,7 +16,7 @@
 	class={`group text-slate-900 relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4 py-2 font-bold transition-transform eas-out hover:scale-105 ${className}`}
 >
 	<span
-		class={`absolute inset-0 z-0 h-full rounded bg-yellow-300 transition-transform duration-300 ease-in-out group-hover:translate-y-0 translate-y-9`}
+		class={`absolute inset-0 z-0 h-full rounded bg-yellow-300 transition-transform duration-300 ease-in-out group-hover:translate-y-0 translate-y-16`}
 	></span>
 
 	<span class="relative flex items-center justify-center gap-2"
