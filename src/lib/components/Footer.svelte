03cf5ac137b68b60f34404164b2351283e74e18f
Nurul Amin <namin2750@gmail.com>
2024-07-30 21:06:07 +0600

    also wrap footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,10 +4,11 @@
 	import IconGithub from '~icons/fa-brands/github';
 	import IconLinkedin from '~icons/fa-brands/linkedin';
 	import IconTwitter from '~icons/simple-icons/x';
+	import Bounded from './Bounded.svelte';
 	export let settings: Content.SettingsDocument;
 </script>
 
-<footer class="text-slate-600">
+<Bounded as="footer" class="text-slate-600">
 	<div
 		class="container mx-auto flex mt-20 flex-col items-center justify-between gap-6 py-8 sm:flex-row"
 	>
@@ -77,4 +78,4 @@
 			{/if}
 		</div>
 	</div>
-</footer>
+</Bounded>
