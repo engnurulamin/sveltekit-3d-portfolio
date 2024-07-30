e2f27f104393f407ec03ecb502b6ab3fe5eb00e6
Nurul Amin <namin2750@gmail.com>
2024-07-30 21:41:21 +0600

    use clsx and heading

--- src/lib/slices/Biography/index.svelte
+++ src/lib/slices/Biography/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Bounded from '$lib/components/Bounded.svelte';
 	import Button from '$lib/components/Button.svelte';
+	import Heading from '$lib/components/Heading.svelte';
 	import type { Content } from '@prismicio/client';
 	import { PrismicImage, PrismicRichText } from '@prismicio/svelte';
 
@@ -9,7 +10,7 @@
 
 <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	<div class="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
-		<h1>{slice.primary.heading}</h1>
+		<Heading size="md" class="col-start-1">{slice.primary.heading}</Heading>
 		<div class="col-start-1 prose prose-invert prose-slate prose-xl">
 			<PrismicRichText field={slice.primary.description} />
 		</div>
