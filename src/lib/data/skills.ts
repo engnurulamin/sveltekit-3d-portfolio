c66a229a57fad99abeddd3f21c845cd5941fbcb6
Nurul Amin <namin2750@gmail.com>
2024-08-13 23:09:37 +0600

    skills col

---
+++ src/lib/data/skills.ts
@@ -0,0 +1,19 @@
+export const skills = [
+	{
+		category: 'Core Skills',
+		items: ['HTML', 'JavaScript', 'CSS', 'PHP', 'MySQL', 'MongoDB', 'Docker', 'Git & GitHub']
+	},
+	{
+		category: 'Libraries & Frameworks',
+		items: [
+			'Svelte & SvelteKit',
+			'React',
+			'Express.js',
+			'Node.js',
+			'Laravel',
+			'Bootstrap',
+			'Bulma',
+			'Sass'
+		]
+	}
+];
--- src/lib/slices/Experience/index.svelte
+++ src/lib/slices/Experience/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Bounded from '$lib/components/Bounded.svelte';
 	import Heading from '$lib/components/Heading.svelte';
+	import { skills } from '$lib/data/skills';
 	import type { Content } from '@prismicio/client';
 	import { PrismicRichText } from '@prismicio/svelte';
 
@@ -32,7 +33,6 @@
 
 <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	<div class="container mx-auto px-4 py-8 grid grid-cols-1 md:grid-cols-2 gap-8">
-		<!-- Left Column: Experience -->
 		<div class="flex flex-col p-8 text-slate-400">
 			<Heading tag="h2" size="md">{slice.primary.heading}</Heading>
 			{#each slice.primary.item as item}
@@ -56,14 +56,24 @@
 			{/each}
 		</div>
 
-		<!-- Right Column: Technologies I Use -->
 		<div class="flex flex-col p-8 text-slate-400">
-			<Heading tag="h2" size="md">Technologies I Use</Heading>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam nulla, eos vero
-				sequi ea quis, nihil quibusdam cum enim labore voluptatem rerum maxime velit perspiciatis,
-				saepe aliquam ex blanditiis.
-			</p>
+			<Heading tag="h2" size="md">Tech I Use</Heading>
+			<div class="ml-6 mt-8 max-w-prose md:ml-12 md:mt-16">
+				{#each skills as skill}
+					<Heading tag="h3" size="sm" class="border-b-2 border-slate-400 pb-2 mb-2"
+						>{skill.category}</Heading
+					>
+					<ul class="list-disc list-inside text-slate-600 space-y-2">
+						{#each skill.items as item}
+							<li
+								class="mt-1 flex w-fit items-center gap-1 text-1xl font-semibold tracking-light text-slate-400"
+							>
+								{item}
+							</li>
+						{/each}
+					</ul>
+				{/each}
+			</div>
 		</div>
 	</div>
 </Bounded>
