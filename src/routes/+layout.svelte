8e69ad48a0898d5566a53dc7b94dea9d2d8ead6e
Nurul Amin <namin2750@gmail.com>
2024-08-14 17:04:11 +0600

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,7 @@
 </main>
 <div class="background-gradient absolute inset-0 -z-50 max-h-screen"></div>
 <div
-	class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"
+	class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('noisetexture.jpg')] opacity-20 mix-blend-soft-light"
 ></div>
 <Footer settings={data.settings} />
 <PrismicPreview {repositoryName} />
